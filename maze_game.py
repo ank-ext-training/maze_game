e7b6530442563e79d6c54e34748bd9aaf31fefa1
rfm110 <malikrubab@gmail.com>
2017-01-20 09:52:26 -0500

    Update maze_game.py

--- maze_game.py
+++ maze_game.py
@@ -1,21 +1,22 @@
 import numpy as np
 def legal_move(move, current_1, current_2, maze):
-    if move.lower() == "up":
-        if maze[current_1-1, current_2] != '#':
-            new_position = current_1-1, current_2
-            return True, new_position
-    if move.lower() == "down":
-        if maze[current_1 + 1, current_2] != '#':
-            new_position = current_1 + 1, current_2
-            return True, new_position
-    if move.lower() == "right":
-        if maze[current_1, current_2 + 1] != '#':
-            new_position = current_1, current_2 + 1
-            return True, new_position
-    if move.lower() == "left":
-        if maze[current_1, current_2 - 1] != '#':
-            new_position = current_1, current_2 - 1
-            return True, new_position
+    possibilities = ["up", "down", "right", "left", '#']
+    if move.lower() == possibilities[0]:
+        current_1 = current_1-1
+    if move.lower() == possibilities[1]:
+        current_1 = current_1 + 1
+
+    if move.lower() == possibilities[2]:
+        current_2 = current_2 + 1
+
+    if move.lower() == possibilities[3]:
+        current_2 = current_2 - 1
+    if maze[current_1, current_2] != '#':
+        new_position = current_1, current_2
+        return True, new_position
+    else:
+        return False, 0
+
 # def maze_step(move, current_1, current_2, new_maze, end_1, end_2, turn):
 #     legal, new_position = legal_move(move, current_1, current_2, new_maze)
 #     if legal == True:
@@ -25,12 +26,10 @@ def legal_move(move, current_1, current_2, maze):
 #         current_pos_1 = new_position[0]
 #         current_pos_2 = new_position[1]
 #         print new_maze
-#
 #         if new_maze[end_1, end_2] != '.':
 #             maze_game(new_maze, turn + 1, current_pos_1, current_pos_2)
 #         else:
 #             print "You win!"
-#
 #         return legal
 
 def maze_game(maze, turn =1, current_1 =0, current_2 = 0):
@@ -72,6 +71,19 @@ def maze_game(maze, turn =1, current_1 =0, current_2 = 0):
 
         break
     return new_maze
+
+maze = np.array([['#', '#', 'E', '#', '#'],
+                          [' ', ' ', ' ', '#', '#'],
+                          ['#', ' ', '#', '#', '#'],
+                          [' ', ' ', '#', ' ', '#'],
+                          ['#', ' ', ' ', ' ', '#'],
+                     ['#', ' ', '#', ' ', '#'],
+                     [' ', ' ', '#', ' ', '#'],
+                     ['#', '#', ' ', ' ', '#'],
+                     ['#', ' ', ' ', '#', '#'],
+                     ['#', ' ', ' ', ' ', 'S']])
+
+maze_game(maze)
 
 maze = np.array([['#', '#', 'E', '#', '#'],
                           [' ', ' ', ' ', '#', '#'],
