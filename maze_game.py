c6840b99c783a8c513336b01ea647e1e340303f0
rfm110 <malikrubab@gmail.com>
2017-01-20 10:56:33 -0500

    Update maze_game.py

--- maze_game.py
+++ maze_game.py
@@ -5,10 +5,8 @@ def legal_move(move, current_1, current_2, maze):
         current_1 = current_1-1
     if move.lower() == possibilities[1]:
         current_1 = current_1 + 1
-
     if move.lower() == possibilities[2]:
         current_2 = current_2 + 1
-
     if move.lower() == possibilities[3]:
         current_2 = current_2 - 1
     if maze[current_1, current_2] != '#':
@@ -17,73 +15,36 @@ def legal_move(move, current_1, current_2, maze):
     else:
         return False, 0
 
-# def maze_step(move, current_1, current_2, new_maze, end_1, end_2, turn):
-#     legal, new_position = legal_move(move, current_1, current_2, new_maze)
-#     if legal == True:
-#         print new_position[0]
-#         print new_position[1]
-#         new_maze[new_position[0], new_position[1]] = '.'
-#         current_pos_1 = new_position[0]
-#         current_pos_2 = new_position[1]
-#         print new_maze
-#         if new_maze[end_1, end_2] != '.':
-#             maze_game(new_maze, turn + 1, current_pos_1, current_pos_2)
-#         else:
-#             print "You win!"
-#         return legal
+def maze_step(move, current_1, current_2, new_maze, end_1, end_2):
+    legal, new_position = legal_move(move, current_1, current_2, new_maze)
+    if legal == True:
+        new_maze[new_position[0], new_position[1]] = '.'
+        current_1 = new_position[0]
+        current_2 = new_position[1]
+        return legal, new_maze, current_1, current_2
 
-def maze_game(maze, turn =1, current_1 =0, current_2 = 0):
+    else:
+        return False, new_maze, current_1, current_2
 
-    if turn == 1:
-        start_1 = str(np.where(maze=='S'))[8]
-        start_2 = str(np.where(maze=='S'))[20]
-        current_1 = int(start_1)
-        current_2 = int(start_2)
-
+def maze_game(maze, current_1 =0, current_2 = 0):
+    start_1 = str(np.where(maze=='S'))[8]
+    start_2 = str(np.where(maze=='S'))[20]
+    current_1 = int(start_1)
+    current_2 = int(start_2)
     end_1 = str(np.where(maze == 'E'))[8]
     end_2 = str(np.where(maze == 'E'))[20]
-
-    print maze
-
     new_maze = maze.copy()
-    game_finish = False
 
-    while game_finish == False:
-
-        move = raw_input("Where would you like to move (up, down, left, or right)?")
-        # legal, new_position = legal_move(move, current_1, current_2, new_maze)
-
-        legal, new_position = legal_move(move, current_1, current_2, maze)
-        if legal != True:
-            print "That is an impossible move, please select a new move"
-            continue
-        print new_position[0]
-        print new_position[1]
-        new_maze[new_position[0], new_position[1]] = '.'
-        current_pos_1 = new_position[0]
-        current_pos_2 = new_position[1]
+    while new_maze[end_1, end_2] != '.':
+        print "Current Game State:"
         print new_maze
-
-        if new_maze[end_1, end_2] != '.':
-            maze_game(new_maze, turn + 1, current_pos_1, current_pos_2)
-        else:
-            print "Maze completed!"
-
-        break
+        move = raw_input("Where would you like to move (up, down, left, or right)? \n")
+        legal, new_maze, current_1, current_2 = maze_step(move, current_1, current_2, new_maze, end_1, end_2)
+        if not legal:
+            print "\n That is an impossible move, please select a new move"
+            continue
+    print "Maze Completed!"
     return new_maze
-
-maze = np.array([['#', '#', 'E', '#', '#'],
-                          [' ', ' ', ' ', '#', '#'],
-                          ['#', ' ', '#', '#', '#'],
-                          [' ', ' ', '#', ' ', '#'],
-                          ['#', ' ', ' ', ' ', '#'],
-                     ['#', ' ', '#', ' ', '#'],
-                     [' ', ' ', '#', ' ', '#'],
-                     ['#', '#', ' ', ' ', '#'],
-                     ['#', ' ', ' ', '#', '#'],
-                     ['#', ' ', ' ', ' ', 'S']])
-
-maze_game(maze)
 
 maze = np.array([['#', '#', 'E', '#', '#'],
                           [' ', ' ', ' ', '#', '#'],
